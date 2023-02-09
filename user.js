0449f7f4bc9f13116c0ec38532bf6c6e94a5f9ef
ElMcFly <104601365+ElMcFly@users.noreply.github.com>
2023-02-08 20:12:08 -0500

    Delete user.js

--- user.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-const User = mongoose.model('User', {
-    email: {type: String, require: true},
-    password: {type: String, require: true},
-    salt:{type: String, require: true}
-});
-
-module.exports = User
